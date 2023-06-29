147ee5c9d51ec287eab2ba9f33d22fe1b31ec38a
tyb03090 <tyb03090@naver.com>
2023-06-29 10:02:50 +0900

    이차원 배열

--- src/p03/TwoArray.java
+++ src/p03/TwoArray.java
@@ -7,19 +7,22 @@ public class TwoArray {
 		
 		System.out.println(numsArray.length);
 		
+//		int c = 0;
+		
 		for(int i = 0; i < numsArray.length; i++) {
 //			System.out.println(numsArray[i].length);
 			for(int j = 0; j < numsArray[i].length; j++) {
-				numsArray[i][j] = 1;
+//				c++;
+//				numsArray[i][j] = c;
+				numsArray[i][j] = numsArray[i][j] + 1 * 2;
 			}
 		}
 		
 		for(int i = 0; i < numsArray.length; i++) {
 			for(int j = 0; j < numsArray[i].length; j++) {
-				System.out.print(numsArray[i][j]);
+				System.out.print("[" + numsArray[i][j] + "]");
 			}
 		}
 	}
-
 }
 
